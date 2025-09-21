84d96b9549aec10ebcd77fdc82d498486b16a807
StewKI <andrijastev4@gmail.com>
2025-09-21 15:16:06 +0200

    MIGRATION to gluestack-ui v3

---
+++ CustomerApp/app/(tabs)/profile.tsx
@@ -0,0 +1,49 @@
+import {Text} from "@/components/ui/text";
+import {useCustomerStore} from "@/services/state/CustomerState";
+import {useEffect, useState} from "react";
+import CustomerBackend from "@/services/models/customer/CustomerBackend";
+import {FullScreenLoader} from "@/components/custom/FullScreenLoader";
+import CustomerCard from "@/components/models/customer/CustomerCard";
+
+
+function ProfileTab() {
+  
+  const { customer, setCustomer } = useCustomerStore();
+
+  const [loading, setLoading] = useState<boolean>(true)
+  const [error, setError] = useState<string | null>(null)
+  
+  const load = async () => {
+    setLoading(true)
+    setError(null);
+    try {
+      const loaded = await CustomerBackend.GetProfile();
+      setCustomer(loaded);
+    }
+    catch (err) {
+      console.error(err);
+      setError("Greska pri ucitavanju");
+    }
+    finally {
+      setLoading(false)
+    }
+  }
+  
+  useEffect(() => {
+    load()
+  }, []);
+  
+  return (
+    <>
+      {loading && <FullScreenLoader/>}
+      {error && <Text>{error}</Text>}
+
+      {customer && (
+        <CustomerCard customer={customer}/>
+      )}
+      
+    </>
+  );
+}
+
+export default ProfileTab;
---
+++ CustomerApp/components/models/product/ProductCard.tsx
@@ -0,0 +1,79 @@
+import {Product, ProductWithDiscounts} from "@/services/types";
+import { Button, ButtonText } from "@/components/ui/button"
+import { Card } from "@/components/ui/card"
+import { Heading } from "@/components/ui/heading"
+import { Image } from "@/components/ui/image"
+import { Text } from "@/components/ui/text"
+import { VStack } from "@/components/ui/vstack"
+import {useProductToAddStore} from "@/services/state/CartState";
+import {useMemo} from "react";
+import DiscountUtil from "@/services/models/discount/DiscountUtil";
+import ProductImageBadges from "@/components/models/product/ProductImageBadges";
+import {Pressable} from "@/components/ui/pressable";
+
+interface ProductCardProps {
+  product: ProductWithDiscounts;
+}
+
+function ProductCard({product}: ProductCardProps) {
+  
+  const {setProduct} = useProductToAddStore();
+  
+  const isDiscounted = useMemo(() => {
+    return product.discounts.length > 0;
+  }, [product])
+  
+  const discountedPrice = useMemo(() => {
+    return DiscountUtil.applyMany(product.price, product.discounts);
+  }, [product])
+
+  return (
+    <Pressable
+      onPress={() => {
+        setProduct(product);
+      }}
+      style={{flexGrow: 1}}
+    >
+      {({ pressed }) => (
+        <Card 
+            size="md" 
+            variant="elevated" 
+            style={[
+                {
+                    flexGrow: 1,
+                },
+                pressed ? {
+                    backgroundColor: "#DDDDDD"
+                } : {}
+            ]}
+        >
+          <VStack space="md">
+
+            <ProductImageBadges product={product}/>
+
+            {isDiscounted ? (
+              <VStack>
+                <Text size="sm" strikeThrough>
+                  {product.price.toFixed(2)} RSD
+                </Text>
+                <Heading size="lg">
+                  {discountedPrice.toFixed(2)} RSD
+                </Heading>
+              </VStack>
+            ) : (
+              <Heading size="lg">
+                {product.price.toFixed(2)} RSD
+              </Heading>
+            )}
+
+            <Text size="md">{product.name}</Text>
+
+          </VStack>
+        </Card>
+      )}
+      
+    </Pressable>
+  );
+}
+
+export default ProductCard;
